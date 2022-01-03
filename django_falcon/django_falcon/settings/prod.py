a1ed8abc688b0019ae1294bbdcfd1e9206a16871
Luke Johansen <johansenluke@yahoo.com>
2022-01-03 08:56:58 -0800

    Initial Commit

---
+++ django_falcon/django_falcon/settings/prod.py
@@ -0,0 +1,9 @@
+from .dev import *
+from dotenv import find_dotenv
+load_dotenv(find_dotenv('.env.prod'), override=True)
+
+DEBUG = False
+DATABASES['default']['NAME'] = os.getenv('DB_NAME')
+DATABASES['default']['USER'] = os.getenv('DB_USER')
+DATABASES['default']['PASSWORD'] = os.getenv('DB_PASSWORD')
+DATABASES['default']['HOST'] = os.getenv('DB_HOST')
